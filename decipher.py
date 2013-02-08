f8d50baf8309200cd13be6fe3da70a0e1f2ec297
blacksleek <tan.phengruey.ryan@dhs.sg>
2013-02-08 12:01:06 +0800

    added comments

--- decipher.py
+++ decipher.py
@@ -13,17 +13,17 @@ def decrypt(file):
 def most_common_3():
     word_list = []
     for line in decrypt.readlines():
-        for word in line.split(" "):
+        for word in line.split(" "): #extract words from lines
             word_list.append(word)
 
     counter = 0
-    for i in Counter(word_list).most_common()[:20]:
+    for i in Counter(word_list).most_common()[:20]: #python library function
         if counter == 3:
             break
-        elif len(i[0]) > 4:
+        elif len(i[0]) > 4: #removing non-nouns (eg. a, the, we)
             outfile.write(i[0] + " ")
             counter = counter + 1
 
 infile.close()
-test.close()
+decrypt.close()
 outfile.close()
